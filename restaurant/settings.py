73bd2d7ae116dc5f901f768701c9f2f3f1e1d7ac
joshuaknight <g.joshuaknight01@gmail.com>
2016-07-29 12:30:04 -0400

    contact app

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -110,4 +110,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+)
+
+STATICFILES_FINDERS = ( 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
